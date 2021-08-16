54b8758cd069363d13325b1afc03d4a7e7d86548
daseuls <dls2tmfs2@naver.com>
2021-08-16 17:53:03 +0900

    Add: component style 추가

--- src/pages/components/VideoItem/VideoItem.jsx
+++ src/pages/components/VideoItem/VideoItem.jsx
@@ -1,12 +1,49 @@
 import React from "react"
+import styled from "styled-components"
 
-export default function VideoItem(props) {
-  // console.log(props.url)
+export default function VideoItem({ video: { snippet } }) {
   return (
-    <li>
-      <img src={props.thumbnail.url} alt="thumbnail" />
-      <p>{props.title}</p>
-      <p>{props.channelTitle}</p>
-    </li>
+    <Container>
+      <Video>
+        <Thumbnail src={snippet.thumbnails.medium.url} alt="thumbnail" />
+        <MetaData>
+          <Title>{snippet.title}</Title>
+          <Channel>{snippet.channelTitle}</Channel>
+        </MetaData>
+      </Video>
+    </Container>
   )
 }
+
+const Container = styled.li`
+  width: 50%;
+  padding: 0.2em;
+`
+const Video = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid lightgrey;
+  cursor: pointer;
+  box-shadow: 3px 3px 5px 0px rgba(191, 191, 191, 0.53);
+  transition: transform 250ms ease-in;
+  &:hover {
+    transform: scale(1.02);
+  }
+`
+const Thumbnail = styled.img`
+  width: 40%;
+  height: 100%;
+`
+
+const MetaData = styled.div`
+  margin-left: 0.2em;
+`
+const Title = styled.p`
+  margin: 0;
+  font-size: 0.8rem;
+`
+
+const Channel = styled.p`
+  margin: 0;
+  font-size: 0.6rem;
+`
--- src/pages/components/VideoList/VideoList.jsx
+++ src/pages/components/VideoList/VideoList.jsx
@@ -1,22 +1,23 @@
 import React from "react"
 import VideoItem from "../VideoItem/VideoItem"
+import styled from "styled-components"
 
 const VideoList = (props) => {
   // console.log("썸네일", props.videoList[0].snippet.thumbnails.medium.url)
   return (
-    <>
+    <Videos>
       {props.videoList.map((item) => {
-        return (
-          <VideoItem
-            key={item.id}
-            title={item.snippet.title}
-            thumbnail={item.snippet.thumbnails.medium}
-            channelTitle={item.snippet.channelTitle}
-          />
-        )
+        return <VideoItem key={item.id} video={item} />
       })}
-    </>
+    </Videos>
   )
 }
 
 export default VideoList
+
+const Videos = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  padding-left: 0;
+`
